a1167df958fbc0bab1aa08811dc5df5048d2e372
Ercan Gerçek <ercan.gercek@redpine.local>
2020-04-07 14:10:52 +0300

    updated build script to install deps

--- scripts/buildandrun.sh
+++ scripts/buildandrun.sh
@@ -2,7 +2,9 @@
 echo "Building client ..."
 
 # build front end project
-cd ../client && npm run build
+cd ../client 
+npm install
+npm run build
 if [ $? -ne 0 ]
 then
   echo "Client Build failed";
@@ -17,7 +19,8 @@ echo "Done."
 
 # start run while loop with nohup
 cd ../server
-sudo nohup ..scripts/util/whilerun.sh > server-log.txt 2>&1 &
+npm install
+nohup ..scripts/util/whilerun.sh > server-log.txt 2>&1 &
 cd -
 
 echo "Server will start shortly in the background."
