71d4e90e5061f4a2ebb3ef192f573f71c9ac7e94
Orçun Yeniay <orcun.yeniay@redpine.local>
2020-04-08 16:03:59 +0300

    updated serialportcontroller

--- server/src/SerialPortController.js
+++ server/src/SerialPortController.js
@@ -2,7 +2,8 @@ const serialport = require('serialport');
 
 
 class SerialPortController {
-  constructor() {
+  constructor({ sendMessageCallback }) {
+    this.sendMessageCallback = sendMessageCallback;
     /*[
       {
         path: 'COM7',
@@ -17,102 +18,114 @@ class SerialPortController {
     ]*/
 
     this.ports = [];
-    /*this.portStatusArray = {
+    /*this.portStatusObj = {
       "COM7": {isOpen: false},
       "COM1": {isOpen: false}
     };*/
-    this.portStatusArray = {};
-    //this.port1;
+    this.portStatusObj = {};
+    
   }
 
-  /* ${ports.path} */
-  //list currently active serial ports
+  
+  init() { }
+
+  getCopyState() {
+    return {
+      ports: { ...this.ports },
+      portStatus: { ...this.portStatusObj },
+    }
+  }
+
+  appendData(obj) {
+    // this function returns the initial state
+    obj["serial"] = this.getCopyState();
+  }
+
+  UpdatePortStatus(){
+    let obj = {};
+    this.appendData(obj);
+    this.sendMessageCallback(obj);
+  }
+//list currently active serial ports
   async listPorts() {
     this.ports = [];
     let ports = await serialport.list();
     this.ports = ports;
 
-    let statusList = Object.keys(this.portStatusArray);
+    let statusList = Object.keys(this.portStatusObj);
 
-    // delete removed port items from portStatusArray
+    // delete removed port items from portStatusObj
     statusList.forEach(port => {
       let devicePort = this.ports.find(item => item.path == port)
-      if(devicePort == undefined) {
-        delete this.portStatusArray[port];
+      if (devicePort == undefined) {
+        delete this.portStatusObj[port];
       }
     })
 
-    // add new items to portStatusArray
+    // add new items to portStatusObj
     this.ports.forEach(item => {
-      if(this.portStatusArray[item.path] == undefined) {
-        this.portStatusArray[item.path] = {isOpen: false};
+      if (this.portStatusObj[item.path] == undefined) {
+        this.portStatusObj[item.path] = { isOpen: false };
       }
     })
-    
-    /*console.log(ports)
-    this.ports = ports;
-    
-    console.log(this.ports[0].path)*/
+    console.log(ports)
+    this.UpdatePortStatus()
+
   }
-  
+
   //open serial port console log parsed serial data
   openSerialPort(devicePath, baudRate = 115200) {
-    // TODO: add number and string check
-    const port = new serialport(devicePath, {baudRate});
+    //number and string check
+    if (typeof devicePath != 'string' || typeof baudRate != 'number') {
+      console.log("invalid parameters",devicePath,baudRate)
+      return
+    }
 
-    // TODO: handle error case
-
+    const port = new serialport(devicePath, { baudRate });
+ 
     port.on('open', this.onPortOpened.bind(this, port));
     port.on('close', this.onPortClosed.bind(this, port));
+    port.on('error', function (err) {
+      console.log('Error: ', err.message)
+    })
 
+    
     // create parser
     let Readline = serialport.parsers.Readline; // make instance of Readline parser
     let parser = new Readline(); // make a new parser to read ASCII lines
     port.pipe(parser);
     parser.on('data', this.onPortDataReceived.bind(this, port));
 
-    /*
-    const port1 = new serialport('COM7', {
-      baudRate: 57600
-    })
-    this.port1 = port1;
-    port1.on('open', this.showPortOpen);
-
-    //parser
-    let Readline = serialport.parsers.Readline; // make instance of Readline parser
-    let parser = new Readline(); // make a new parser to read ASCII lines
-    port1.pipe(parser);
-    parser.on('data', console.log)
-
-    port1.on('close', this.showPortClose);*/
-  }
+    }
 
   onPortOpened(port) {
-    // Find and update port list item
-    // let myItem = this.ports.find(item => item.path == port.path);
-    console.log('port1 open.', port);
 
+    this.portStatusObj[port.path] = true;
+    console.log('port open.', port.path);
+    this.UpdatePortStatus()
+    
     // Send data to clients
     // { serialport: { type: "portOpened", portPath: ""  } }
   }
 
   onPortDataReceived(port, data) {
     // Find and update port list item
-    console.log('port data received',port,data);
-
+    console.log('port data received', data);
+    
     // Send data to clients
     // { serialport: { type: "portDataReceived", data: ""  } }
   }
 
   onPortClosed(port) {
     // Find and update port list
-    console.log('port closed.', port);
-
+    this.portStatusObj[port.path] = false;
+    console.log('port closed.', port.path);
+    this.UpdatePortStatus()
     // Send data to clients
     // { serialport: { type: "portClosed", portPath: ""  } }
   }
 
-  
+
 }
 
 
--- server/src/main.js
+++ server/src/main.js
@@ -1,8 +1,12 @@
 const GPIOController = require('./GPIOController');
 const AppServer = require('./AppServer');
+const SerialPortController = require('./SerialPortController');
+
+
 
 dataHandlers = [];
 dataHandlers.push(new GPIOController({sendMessageCallback}));
+dataHandlers.push(new SerialPortController({sendMessageCallback}));
 
 // create connection manager
 const appServer = new AppServer({dataHandlers});
