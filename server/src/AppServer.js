3f54c4eacca7465235d06b6b426073dca71404d1
Deniz Kinay <deniz.kinay@redpine.com.tr>
2020-05-11 08:58:30 +0300

    eslint --fix on AppServer.js

--- server/src/AppServer.js
+++ server/src/AppServer.js
@@ -10,7 +10,7 @@ class AppServer {
   constructor({ dataHandlers }) {
     this.dataHandlers = dataHandlers;
     this.port = 3000;
-    if (process.argv[2] === "production") {
+    if (process.argv[2] === 'production') {
       // change the port to 80 as this is running in production mode
       this.port = 80;
     }
@@ -21,13 +21,13 @@ class AppServer {
   }
 
   init() {
-    console.log("initializing AppServer...");
+    console.log('initializing AppServer...');
     this.app = express();
 
     // start web server
-    this.app.use(bodyParser.json({limit: "500mb"}));
+    this.app.use(bodyParser.json({ limit: '500mb' }));
     this.app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
-    this.app.use(bodyParser.raw({limit: "500mb"}));
+    this.app.use(bodyParser.raw({ limit: '500mb' }));
     this.app.use(compression());
 
     this.httpServer = http.createServer(this.app);
@@ -37,15 +37,14 @@ class AppServer {
     this.app.use(express.static('public'));
 
     this.app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
       next();
     });
 
     // create websocket server
     this.webSocketServer = new WebSocketServer({ server: this.httpServer });
     this.webSocketServer.on('connection', this.onConnectionHandler.bind(this));
-
   }
 
   getApp() {
@@ -55,10 +54,10 @@ class AppServer {
   onConnectionHandler(connection, req) {
     // request handler
     const id = uuidv4();
-    const isAuthenticated = false
-    console.log("New connection request received! id: ", id);
+    const isAuthenticated = false;
+    console.log('New connection request received! id: ', id);
     const ip = req.connection.remoteAddress;
-    console.log("Remote client address:",ip)
+    console.log('Remote client address:', ip);
     const client = {
       id,
       connection,
@@ -66,8 +65,8 @@ class AppServer {
       ip,
       userObject: null,
       send: (obj) => {
-        connection.send(JSON.stringify(obj))
-      }
+        connection.send(JSON.stringify(obj));
+      },
     };
     this.clients.push(client);
 
@@ -79,28 +78,28 @@ class AppServer {
   }
 
   onMessageHandler(client, message) {
-    console.log("client isAuthenticated",client.isAuthenticated)
-    let obj = JSON.parse(message);
+    console.log('client isAuthenticated', client.isAuthenticated);
+    const obj = JSON.parse(message);
     this.dataHandlers.forEach((handler) => {
-      if(!handler.isAuthRequired() || (handler.isAuthRequired() && client.isAuthenticated)) {
+      if (!handler.isAuthRequired() || (handler.isAuthRequired() && client.isAuthenticated)) {
         handler.handleMessage(obj, client);
       }
     });
   }
 
   onCloseHandler(client, connection) {
-    console.log("connection closed! id: ", client.id);
-    let index = this.clients.indexOf(client);
-    if (index != -1) {
+    console.log('connection closed! id: ', client.id);
+    const index = this.clients.indexOf(client);
+    if (index !== -1) {
       this.clients.splice(index, 1);
     } else {
-      console.log("Error on closing connection! id: ", client.id);
+      console.log('Error on closing connection! id: ', client.id);
     }
   }
 
   sendInitialMessage(client) {
     // collect data from data handlers
-    let obj = {};
+    const obj = {};
     this.dataHandlers.forEach((handler) => {
       handler.appendData(obj);
     });
