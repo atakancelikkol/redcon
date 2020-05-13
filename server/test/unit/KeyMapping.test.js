bcd8db6ccdb81f8bfe50c5d939a2bf611ee3632b
Atakan Celikkol <atakan.celikkol@redpine.local>
2020-05-13 15:47:46 +0300

    keymaptest + coverage config added

---
+++ server/test/unit/KeyMapping.test.js
@@ -0,0 +1,8 @@
+const KeyMapping = require('../../src/util/KeyMapping');
+
+describe("KeyMapping", () => {
+  it("ConvertKey of KeyMapping", () => {
+    //const dataToSend = KeyMapping.ConvertKey(keyCode, charCode, ctrlKey, shiftKey);
+    expect(1).toStrictEqual(1);
+  });
+});
