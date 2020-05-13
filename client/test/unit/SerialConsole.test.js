7a79651d16444f09d1cb9811b4b639b9cada2f4e
Orçun Yeniay <orcun.yeniay@redpine.local>
2020-05-13 14:12:33 +0300

    click event test added

--- client/test/unit/SerialConsole.test.js
+++ client/test/unit/SerialConsole.test.js
@@ -1,8 +1,44 @@
+import Vuex from "vuex"
+import { shallowMount, createLocalVue, mount } from "@vue/test-utils"
 import ComponentWithVuex from '../../src/components/SerialConsole.vue'
-import store from '../helpers/StoreHelper.js'
+import  BootstrapVue, { BButton } from 'bootstrap-vue'
 
+//import { mapState, mapActions } from "vuex";
+import sinon from 'sinon'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(BootstrapVue);
+
+const store = new Vuex.Store({
+  state: {
+    //mapState
+    receivedData: { serial: { serialFiles: {} } },
+
+    serialmsg: 'msg',
+
+    serialData: "asdasdasdasd",
+
+    currentSerialDevice: null,
+
+    baudRate: 115200
+  },
+  actions: {
+    //mapActions
+
+    openSerialDevice: jest.fn(),
+    closeSerialDevice: jest.fn(),
+    listSerialDevices: jest.fn(),
+    writeSerialDevice: jest.fn(),
+    writeKeySerialDevice: jest.fn(),
+
+  },
+})
 
 describe("ComponentWithVuex", () => {
+
+ 
+
 
   it('default baundRate', () => {
 
@@ -16,4 +52,35 @@ describe("ComponentWithVuex", () => {
   })
 
 
+
+
+
+
+
+
+  it("click event for openSelectedDevice", () => {
+    const openSelectedDeviceSpy = sinon.spy();
+    const wrapper = mount(ComponentWithVuex,{
+      store,
+      localVue,
+      methods: {
+        openSelectedDevice: openSelectedDeviceSpy,
+      },
+      
+    })
+    //const methodStub = sinon.spy(wrapper.vm, 'openSelectedDevice');
+  
+    
+    //click event
+       const button = wrapper.findComponent(BButton)
+       button.trigger('click') 
+
+
+    //wrapper.vm.openSelectedDevice()
+    
+
+
+    expect(openSelectedDeviceSpy.called).toBe(true)
+    //spy.should.have.been.calledWith(currentSerialDevice,baudRate)
+  })
 })
