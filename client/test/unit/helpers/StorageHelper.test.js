82a640319fd4a818c61f8a55e0d363a1fc09eb58
Orçun Yeniay <orcun.yeniay@redpine.local>
2020-05-20 15:19:42 +0300

    storagehelper test

---
+++ client/test/unit/helpers/StorageHelper.test.js
@@ -0,0 +1,31 @@
+
+import StorageHelper from '../../../src/helpers/StorageHelper';
+
+describe("GPIOController", () => {
+
+  beforeEach(() => {
+  StorageHelper.clearall()
+  })
+  
+  it("Storage helper getItem", () => {
+    localStorage.testTag='testItem'
+    let item =StorageHelper.getItem('testTag')
+    expect(item).toBe('testItem');
+  });
+
+  it("Storage helper setItem", () => {
+  StorageHelper.setItem('testTag','testItem')
+  expect(localStorage.testTag).toBe('testItem');
+  });
+
+  it("Storage helper removeItem", () => {
+  StorageHelper.removeItem('testTag')
+  expect(localStorage.testTag).toBe(undefined);
+  });
+
+  it("Storage helper clearall", () => {
+  StorageHelper.clearall
+  expect(localStorage).toMatchObject({});
+  });
+
+});
