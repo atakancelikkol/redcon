14e9e39e23d87e4bbfa3cc24690cf2751317b500
Deniz Kinay <deniz.kinay@redpine.com.tr>
2020-05-22 14:51:16 +0300

    started to coding unit tests for SerialPortController.js

--- server/test/unit/SerialPortController.test.js
+++ server/test/unit/SerialPortController.test.js
@@ -1,10 +1,84 @@
 const SerialPortController = require('../../src/SerialPortController');
-const serialPortController = new SerialPortController({});
-
 
 describe("SerialPortController", () => {
+  test("constructor", () => {
+    const serialPortController = new SerialPortController({});
+    expect(serialPortController.virtualDeviceInterval).toBeUndefined();
+  });
+
   test("should return true", () => {
     expect(serialPortController.isAuthRequired()).toBe(true);
   });
 
+  test("init does nothing", () => {
+
+  });
+
+  test("readOutputFiles", () => {
+
+  });
+
+  test("startVirtualDevice", () => {
+
+  });
+
+  test("stopVirtualDevice", () => {
+
+  });
+
+  test("getCopyState", () => {
+
+  });
+
+  test("appendData", () => {
+
+  });
+
+  test("updatePortStatus", () => {
+
+  });
+
+  test("handleMessage", () => {
+
+  });
+
+  test("listPorts", () => {
+
+  });
+
+  test("writeSerialPort", () => {
+
+  });
+
+  test("writeKeySerialPort", () => {
+
+  });
+
+  test("closeSerialPort", () => {
+
+  });
+
+  test("openWriteStream", () => {
+
+  });
+
+  test("openSerialPort", () => {
+
+  });
+
+  test("onPortOpened", () => {
+
+  });
+
+  test("onPortDataReceived", () => {
+
+  });
+
+  test("onPortClosed", () => {
+
+  });
+
+  test("onExit doesn nothing", () => {
+
+  });
 });
