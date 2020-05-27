c744a8e1273a9718453bd415301618db322c6895
Orçun Yeniay <orcun.yeniay@redpine.local>
2020-05-27 12:27:15 +0300

    indentation fix

--- client/test/unit/helpers/StorageHelper.test.js
+++ client/test/unit/helpers/StorageHelper.test.js
@@ -4,28 +4,28 @@ import StorageHelper from '../../../src/helpers/StorageHelper';
 describe("GPIOController", () => {
 
   beforeEach(() => {
-  StorageHelper.clearall()
+    StorageHelper.clearall()
   })
-  
+
   it("Storage helper getItem", () => {
-    localStorage.testTag='testItem'
-    let item =StorageHelper.getItem('testTag')
+    localStorage.testTag = 'testItem'
+    let item = StorageHelper.getItem('testTag')
     expect(item).toBe('testItem');
   });
 
   it("Storage helper setItem", () => {
-  StorageHelper.setItem('testTag','testItem')
-  expect(localStorage.testTag).toBe('testItem');
+    StorageHelper.setItem('testTag', 'testItem')
+    expect(localStorage.testTag).toBe('testItem');
   });
 
   it("Storage helper removeItem", () => {
-  StorageHelper.removeItem('testTag')
-  expect(localStorage.testTag).toBe(undefined);
+    StorageHelper.removeItem('testTag')
+    expect(localStorage.testTag).toBe(undefined);
   });
 
   it("Storage helper clearall", () => {
-  StorageHelper.clearall
-  expect(localStorage).toMatchObject({});
+    StorageHelper.clearall()
+    expect(localStorage).toMatchObject({});
   });
 
 });
