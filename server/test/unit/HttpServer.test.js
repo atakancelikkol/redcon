ff1f7ec1a7d2ef07480adddfb2e562c10b3cb803
Atakan Celikkol <atakan.celikkol@redpine.local>
2020-05-27 18:00:43 +0300

    HttpServer onCloseHandler & sendToAllClients tests impl.

--- server/test/unit/HttpServer.test.js
+++ server/test/unit/HttpServer.test.js
@@ -6,23 +6,23 @@ const Authenticator = require('../../src/Authenticator');
 describe("HttpServer ", () => {
 
   it("Constructor", () => {
-    const controllers = [];
-    const httpServer = new HttpServer({ controllers });
+    let controllers = [];
+    let httpServer = new HttpServer({ controllers });
     expect(httpServer.port).toBe(3000);
     expect(httpServer.controllers).toBe(controllers);
   });
 
   it("getApp", () => {
-    const controllers = [];
+    let controllers = [];
     controllers.push(new Authenticator({}));
-    const httpServer = new HttpServer({ controllers });
+    let httpServer = new HttpServer({ controllers });
     expect(httpServer.getApp()).toBe(httpServer.app);
   });
 
 
   describe("init ", () => {
     xit('should invoke express once', () => {
-      const httpServer = new HttpServer({});
+      let httpServer = new HttpServer({});
       httpServer.init();
       console.log(httpServer.getApp());
       expect(httpServer.getApp()).toStrictEqual(tempApp);
@@ -32,26 +32,154 @@ describe("HttpServer ", () => {
     httpServer.init();
     expect(mockExpressUse).toHaveBeenCalled();*/
   });
+  describe("onCloseHandler", () => {
+    it('onCloseHandler if index !== -1', () => {
+      let httpServer = new HttpServer({});
+      const mockClient = {
+        id: 'myConnectionClosingId',
+        getId: () => { return mockClient.id; },
+      }
+      httpServer.clients.push(mockClient);
+      httpServer.onCloseHandler(mockClient);
+      expect(httpServer.clients.length).toBe(0);
+    })
+
+    it('onCloseHandler if index === -1', () => {
+      let httpServer = new HttpServer({});
+      let mockClient = {
+        id: 'myConnectionClosingId',
+        getId: () => { return mockClient.id; },
+      }
+      let log = console.log;
+      console.log = jest.fn();
+      httpServer.onCloseHandler(mockClient);
+      expect(console.log).toHaveBeenCalledWith('Error on closing connection! id: ', mockClient.getId());
+      console.log = log;
+    })
+  });
 
   it('sendInitialMessage', () => {
-    const expectedStringifiedObject = { testMember: 'test' };
-    const controllers = [];
-    const mockController = {
+    const testObject = { testMember: 'test' };
+    let controllers = [];
+    let mockController = {
       appendData: (obj) => { obj.testMember = 'test'; }
     };
     controllers.push(mockController);
-    const httpServer = new HttpServer({ controllers });
-
+    let httpServer = new HttpServer({ controllers });
     let sendObject;
-    const mockConnection = { send: (objStr) => { sendObject = objStr; } };
+    let mockConnection = { send: (objStr) => { sendObject = objStr; } };
     mockClient = {
       connection: mockConnection
-
     };
-
-    httpServer.sendInitialMessage(mockClient)
-    expect(sendObject).toStrictEqual(JSON.stringify(expectedStringifiedObject));
+    httpServer.sendInitialMessage(mockClient);
+    expect(sendObject).toStrictEqual(JSON.stringify(testObject));
   })
+
+  describe("sendToAllClients", () => {
+
+    it('sendToAllClients isAuthReq:0 , isAuthed:0', () => {
+      const testObject = {
+        testMember : 'testString'
+      };
+
+      const mockController = {
+        isAuthRequired: () => { return false; }
+      };
+      const httpServer = new HttpServer({});
+      let sendObject;
+      const mockConnection = { send: (objStr) => { sendObject = objStr; } };
+      const mockClient = {
+        connection: mockConnection,
+        isAuthenticated: () => { return true; },
+        id: 'sendToAllClientsId',
+        getId: () => { return mockClient.id; }
+      };
+      httpServer.clients.push(mockClient);
+      const obj = {
+        testMember : 'testString'
+      };
+      httpServer.sendToAllClients(mockController, obj);
+      expect(sendObject).toStrictEqual(JSON.stringify(testObject));
+    });
+
+    it('sendToAllClients isAuthReq:0 , isAuthed:1', () => {
+      const testObject = {
+        testMember : 'testString'
+      };
+
+      const mockController = {
+        isAuthRequired: () => { return false; }
+      };
+      const httpServer = new HttpServer({});
+      let sendObject;
+      const mockConnection = { send: (objStr) => { sendObject = objStr; } };
+      const mockClient = {
+        connection: mockConnection,
+        isAuthenticated: () => { return true; },
+        id: 'sendToAllClientsId',
+        getId: () => { return mockClient.id; }
+      };
+      httpServer.clients.push(mockClient);
+      const obj = {
+        testMember : 'testString'
+      };
+      httpServer.sendToAllClients(mockController, obj);
+      expect(sendObject).toStrictEqual(JSON.stringify(testObject));
+    });
+
+    it('sendToAllClients isAuthReq:1 , isAuthed:1', () => {
+      const testObject = {
+        testMember : 'testString'
+      };
+
+      const mockController = {
+        isAuthRequired: () => { return true; }
+      };
+      const httpServer = new HttpServer({});
+      let sendObject;
+      const mockConnection = { send: (objStr) => { sendObject = objStr; } };
+      const mockClient = {
+        connection: mockConnection,
+        isAuthenticated: () => { return true; },
+        id: 'sendToAllClientsId',
+        getId: () => { return mockClient.id; }
+      };
+      httpServer.clients.push(mockClient);
+      const obj = {
+        testMember : 'testString'
+      };
+      httpServer.sendToAllClients(mockController, obj);
+      expect(sendObject).toStrictEqual(JSON.stringify(testObject));
+    });
+
+    it('sendToAllClients isAuthReq:1 , isAuthed:0', () => {
+
+      const mockController = {
+        isAuthRequired: () => { return true; }
+      };
+      const httpServer = new HttpServer({});
+      let sendObject;
+      const mockConnection = { send: (objStr) => { sendObject = objStr; } };
+      const mockClient = {
+        connection: mockConnection,
+        isAuthenticated: () => { return false; },
+        id: 'sendToAllClientsId',
+        getId: () => { return mockClient.id; }
+      };
+      httpServer.clients.push(mockClient);
+      const obj = {
+        testMember : 'testString'
+      };
+      let log = console.log;
+      console.log = jest.fn();
+      httpServer.sendToAllClients(mockController, obj);
+      expect(console.log).toHaveBeenCalledWith(`Authentication is required for this controller feature and ${mockClient.getId()} is not Authenticated`);
+      console.log = log;
+    });
+
+  });
+
+
 
 });
 
