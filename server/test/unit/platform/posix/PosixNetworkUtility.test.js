513174e0524a62af1c5d83f6cde727cbde6d883b
Ercan Gerçek <ercan.gercek@redpine.local>
2020-05-28 19:04:15 +0300

    added callback test

--- server/test/unit/platform/posix/PosixNetworkUtility.test.js
+++ server/test/unit/platform/posix/PosixNetworkUtility.test.js
@@ -1,12 +1,22 @@
 const PosixNetworkUtility = require('../../../../src/platform/posix/PosixNetworkUtility');
 const { exec } = require('child_process');
 
-jest.mock('child_process');
+let execCommandString = '';
+jest.mock('child_process', () => ({
+  exec: jest.fn((commandString, callback) => {
+    execCommandString = commandString;
+    callback('error', 'testStdOut', 'testStdError');
+  })
+}));
 
 describe("PosixNetworkUtility test", () => {
-  it("exec should called once", () => {
+  it("exec should called once", done => {
     const posixNetworkUtility = new PosixNetworkUtility();
-    posixNetworkUtility.run()
-    expect(exec).toHaveBeenCalled();
+    posixNetworkUtility.run().then((platformPortConfig)=>{
+      expect(platformPortConfig).toStrictEqual({shellError: 'testStdError', shellOutput: 'testStdOut'});
+      done();
+    });
+
+    expect(exec.mock.calls[0][0]).toBe(execCommandString);
   });
 });
