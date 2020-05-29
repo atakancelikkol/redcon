5b00c3c0378ec159cf9da7fad9c72483c07c88e0
Nurettin Mert Dolapcı <mert.dolapci@redpine.local>
2020-05-29 10:45:44 +0300

    Removed log message

--- server/src/platform/mock/MockNetworkUtility.js
+++ server/src/platform/mock/MockNetworkUtility.js
@@ -2,7 +2,6 @@ const INetworkUtility = require('../interfaces/INetworkUtility');
 
 class MockNetworkUtility extends INetworkUtility {
   applyPortConfiguration() {
-    console.log('Port forwarding script can be used only in linux operating system.');
     return new Promise((resolve) => {
       resolve();
     });
