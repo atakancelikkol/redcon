305b0dde6aeb764e3b9e76ef58553fdbef696b95
Ercan Gerçek <ercan.gercek@redpine.local>
2020-05-29 10:54:57 +0300

    review work is done

--- server/test/unit/platform/posix/PosixPlatformUtility.test.js
+++ server/test/unit/platform/posix/PosixPlatformUtility.test.js
@@ -1,6 +1,6 @@
 const PosixPlatformUtility = require('../../../../src/platform/posix/PosixPlatformUtility');
 const { execSync } = require('child_process');
-jest.mock('child_process'); // websocketConnector is now a mock constructor
+jest.mock('child_process');
 
 describe("PosixPlatformUtility test", () => {
   it("check reboot method", () => {
