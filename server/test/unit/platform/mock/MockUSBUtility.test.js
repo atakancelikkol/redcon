8523e7aea0613b5f196b1e549ea78608736a91fd
Orçun Yeniay <orcun.yeniay@redpine.local>
2020-06-08 14:06:28 +0300

    mockusbutility test

--- server/test/unit/platform/mock/MockUSBUtility.test.js
+++ server/test/unit/platform/mock/MockUSBUtility.test.js
@@ -3,7 +3,7 @@ const MockUSBUtility = require('../../../../src/platform/mock/MockUSBUtility');
 describe('MockUSBUtility test', () => new Promise((done) => {
   it('methods should be called without an error', () => {
     const mockUSBUtility = new MockUSBUtility();
-    expect(mockUSBUtility.extractUsbState()).toEqual(true);
+    expect(mockUSBUtility.extractUsbState()).toEqual({"device": "", "isAvailable": true, "mountedPath": "", "usbErrorString": "", "usbName": "testUsbName"});
     expect(mockUSBUtility.syncUsbDevice()).toEqual(true);
     mockUSBUtility.ejectUSBDriveSafely().then(() => {
       done();
