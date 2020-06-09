4e22df5c314689b308004e6b6b9e59bb441700a4
Atakan Celikkol <atakan.celikkol@redpine.local>
2020-06-09 08:56:34 +0300

    client/logger setloglevel impl.

--- client/src/helpers/Logger.js
+++ client/src/helpers/Logger.js
@@ -1,19 +1,41 @@
 
 class Logger {
   constructor() {
-    this.level = undefined;
+    this.level = 0; // default level is set to debug level
   }
 
   info(...args) {
-    console.info(...args); // eslint-disable-line
+    if (this.level <= 1) {
+      console.info(...args); // eslint-disable-line
+    }
   }
 
   error(...args) {
-    console.error(...args); // eslint-disable-line
+    if (this.level <= 2) {
+      console.error(...args); // eslint-disable-line
+    }
   }
 
   debug(...args) {
-    console.debug(...args); // eslint-disable-line
+    if (this.level <= 0) {
+      console.debug(...args); // eslint-disable-line
+    }
+  }
+
+  setLevel(lvl) {
+    switch (lvl) {
+      case 'info':
+        this.level = 1;
+        break;
+      case 'error':
+        this.level = 2;
+        break;
+      case 'debug':
+        this.level = 0;
+        break;
+      default:
+        this.level = 0;
+    }
   }
 }
 
