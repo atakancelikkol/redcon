9dbe955bdc52a42269271d2ba1ab02aaa066c73a
Atakan Celikkol <atakan.celikkol@redpine.local>
2020-06-09 08:57:40 +0300

    client/logger setloglevel test impl.

--- client/test/unit/helpers/Logger.test.js
+++ client/test/unit/helpers/Logger.test.js
@@ -3,7 +3,7 @@ import logger from '../../../src/helpers/Logger';
 
 describe('Logger', () => {
   it('test level', () => {
-    expect(logger.level).toBe(undefined);
+    expect(logger.level).toBe(0);
   });
 
   it('tests info', () => {
@@ -25,5 +25,25 @@ describe('Logger', () => {
     logger.debug('arg', 123);
     expect(spyLogger).toHaveBeenCalledWith('arg', 123);
     spyLogger.mockClear();
+  });
+
+  it('tests changing log level debug', () => {
+    logger.setLevel('debug');
+    expect(logger.level).toBe(0);
+  });
+
+  it('tests changing log level info', () => {
+    logger.setLevel('info');
+    expect(logger.level).toBe(1);
+  });
+
+  it('tests changing log level error', () => {
+    logger.setLevel('error');
+    expect(logger.level).toBe(2);
+  });
+
+  it('tests changing log level default', () => {
+    logger.setLevel('defaultlevel');
+    expect(logger.level).toBe(0);
   });
 });
