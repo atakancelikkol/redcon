9e47c82348ebacb7b727260cdb3df98e48517c74
Ercan Gerçek <ercan.gercek@redpine.local>
2020-06-11 12:19:17 +0300

    added iptables-legacy for compatibility

--- server/src/platform/posix/IPTableRuleGenerator.js
+++ server/src/platform/posix/IPTableRuleGenerator.js
@@ -1,54 +1,55 @@
 const { EOL } = require('os');
 
-function generateRouteCommands(interfaces, interfaceConfiguration) {
+function generateRouteCommands(iptablesCommand, interfaces, interfaceConfiguration) {
   const { internalInterfaceSubnet } = interfaceConfiguration;
   const iptableRule = `# ROUTE RULES - Set as a gateway
 ip route add ${internalInterfaceSubnet} dev ${interfaces.internal.name}
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A POSTROUTING ! -d ${internalInterfaceSubnet} -o ${interfaces.external.name} -j SNAT --to-source ${interfaces.external.ip}
+${iptablesCommand} -t nat -A POSTROUTING ! -d ${internalInterfaceSubnet} -o ${interfaces.external.name} -j SNAT --to-source ${interfaces.external.ip}
 `;
   return iptableRule;
 }
 
-function generateUdpIntToExtRule(interfaces, rule, index) {
+function generateUdpIntToExtRule(iptablesCommand, interfaces, rule, index) {
   // internalIp, internalPort, externalIp
   const iptableRule = `# rule-${index} "${rule.name}" -- UDP INTERNAL to EXTERNAL network
-sudo iptables -t nat -A PREROUTING -i ${interfaces.internal.name} -p udp --dport ${rule.internalPort} -j DNAT --to ${rule.externalIp}
-sudo iptables -t nat -A POSTROUTING -o ${interfaces.external.name} -p udp --dport ${rule.internalPort} -j SNAT --to-source ${interfaces.external.ip}
-sudo iptables -t nat -A POSTROUTING -p udp --sport ${rule.internalPort} -j SNAT --to-source ${rule.internalIp}
+sudo ${iptablesCommand} -t nat -A PREROUTING -i ${interfaces.internal.name} -p udp --dport ${rule.internalPort} -j DNAT --to ${rule.externalIp}
+sudo ${iptablesCommand} -t nat -A POSTROUTING -o ${interfaces.external.name} -p udp --dport ${rule.internalPort} -j SNAT --to-source ${interfaces.external.ip}
+sudo ${iptablesCommand} -t nat -A POSTROUTING -p udp --sport ${rule.internalPort} -j SNAT --to-source ${rule.internalIp}
 `;
   return iptableRule;
 }
 
-function generateUdpExtToIntRule(interfaces, rule, index) {
+function generateUdpExtToIntRule(iptablesCommand, interfaces, rule, index) {
   // externalIp && externalPort && internalIp
   const iptableRule = `# rule-${index} "${rule.name}" -- UDP EXTERNAL to INTERNAL network
-sudo iptables -t nat -A PREROUTING -i ${interfaces.external.name} -p udp --dport ${rule.externalPort} -j DNAT --to ${rule.internalIp}
-sudo iptables -t nat -A POSTROUTING -o ${interfaces.internal.name} -p udp --dport ${rule.externalPort} -j SNAT --to-source ${interfaces.internal.ip}
-sudo iptables -t nat -A POSTROUTING -p udp --sport ${rule.externalPort} -j SNAT --to-source ${rule.externalIp}
+sudo ${iptablesCommand} -t nat -A PREROUTING -i ${interfaces.external.name} -p udp --dport ${rule.externalPort} -j DNAT --to ${rule.internalIp}
+sudo ${iptablesCommand} -t nat -A POSTROUTING -o ${interfaces.internal.name} -p udp --dport ${rule.externalPort} -j SNAT --to-source ${interfaces.internal.ip}
+sudo ${iptablesCommand} -t nat -A POSTROUTING -p udp --sport ${rule.externalPort} -j SNAT --to-source ${rule.externalIp}
 `;
   return iptableRule;
 }
 
-function generateTcpExtToIntRule(interfaces, rule, index) {
+function generateTcpExtToIntRule(iptablesCommand, interfaces, rule, index) {
   // rule -> deviceExternalPort && internalIp && internalPort
   const iptableRule = `# rule-${index} "${rule.name}" -- TCP EXTERNAL to INTERNAL network
-sudo iptables -t nat -A PREROUTING -p tcp -i ${interfaces.external.name} --dport ${rule.deviceExternalPort} -j DNAT --to-destination ${rule.internalIp}:${rule.internalIPort}
-sudo iptables -A FORWARD -p tcp -d ${rule.internalIp} --dport ${rule.internalPort} -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+sudo ${iptablesCommand} -t nat -A PREROUTING -p tcp -i ${interfaces.external.name} --dport ${rule.deviceExternalPort} -j DNAT --to-destination ${rule.internalIp}:${rule.internalIPort}
+sudo ${iptablesCommand} -A FORWARD -p tcp -d ${rule.internalIp} --dport ${rule.internalPort} -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 `;
   return iptableRule;
 }
 
-function generateTcpIntToExtRule(interfaces, rule, index) {
+function generateTcpIntToExtRule(iptablesCommand, interfaces, rule, index) {
   // rule -> deviceInternalPort && externalIp && externalPort
   const iptableRule = `# rule-${index} "${rule.name}" -- TCP EXTERNAL to INTERNAL network
-sudo iptables -t nat -A PREROUTING -p tcp -i ${interfaces.internal.name} --dport ${rule.deviceInternalPort} -j DNAT --to-destination ${rule.externalIp}:${rule.externalIPort}
-sudo iptables -A FORWARD -p tcp -d ${rule.externalIp} --dport ${rule.externalPort} -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+sudo ${iptablesCommand} -t nat -A PREROUTING -p tcp -i ${interfaces.internal.name} --dport ${rule.deviceInternalPort} -j DNAT --to-destination ${rule.externalIp}:${rule.externalIPort}
+sudo ${iptablesCommand} -A FORWARD -p tcp -d ${rule.externalIp} --dport ${rule.externalPort} -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 `;
   return iptableRule;
 }
 
-function generateScript(config) {
+function generateScript(config, useLegacyIpTables = true) {
+  const iptablesCommand = useLegacyIpTables ? 'iptables-legacy' : 'iptables';
   // device external ip
   const interfaces = {};
   interfaces.external = config.networkInterfaces.find((item) => item.name === config.interfaceConfiguration.externalInterfaceName);
@@ -67,29 +68,29 @@ function generateScript(config) {
     tcpExtToIntRules: [],
   };
 
-  iptableCommand.routeCommands = generateRouteCommands(interfaces, config.interfaceConfiguration);
+  iptableCommand.routeCommands = generateRouteCommands(iptablesCommand, interfaces, config.interfaceConfiguration);
 
   iptableCommand.udpIntToExtRules.push('# UDP | INTERNAL network -> EXTERNAL network');
   config.udpIntToExtRules.forEach((rule, index) => {
-    iptableCommand.udpIntToExtRules.push(generateUdpIntToExtRule(interfaces, rule, index));
+    iptableCommand.udpIntToExtRules.push(generateUdpIntToExtRule(iptablesCommand, interfaces, rule, index));
   });
   iptableCommand.udpIntToExtRules.push(`# -----${EOL}`);
 
   iptableCommand.udpExtToIntRules.push('# UDP | EXTERNAL network -> INTERNAL network');
   config.udpExtToIntRules.forEach((rule, index) => {
-    iptableCommand.udpExtToIntRules.push(generateUdpExtToIntRule(interfaces, rule, index));
+    iptableCommand.udpExtToIntRules.push(generateUdpExtToIntRule(iptablesCommand, interfaces, rule, index));
   });
   iptableCommand.udpExtToIntRules.push(`# -----${EOL}`);
 
   iptableCommand.tcpIntToExtRules.push('# TCP | INTERNAL network -> EXTERNAL network');
   config.tcpIntToExtRules.forEach((rule, index) => {
-    iptableCommand.tcpIntToExtRules.push(generateTcpIntToExtRule(interfaces, rule, index));
+    iptableCommand.tcpIntToExtRules.push(generateTcpIntToExtRule(iptablesCommand, interfaces, rule, index));
   });
   iptableCommand.tcpIntToExtRules.push(`# -----${EOL}`);
 
   iptableCommand.tcpExtToIntRules.push('# TCP | EXTERNAL network -> INTERNAL network');
   config.tcpExtToIntRules.forEach((rule, index) => {
-    iptableCommand.tcpExtToIntRules.push(generateTcpExtToIntRule(interfaces, rule, index));
+    iptableCommand.tcpExtToIntRules.push(generateTcpExtToIntRule(iptablesCommand, interfaces, rule, index));
   });
   iptableCommand.tcpExtToIntRules.push(`# -----${EOL}`);
 
