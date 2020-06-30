2763f0d3042ad6ca0318fd29ed3c8b30ec18ce01
Orçun Yeniay <orcun.yeniay@redpine.local>
2020-06-30 14:17:01 +0300

    applyPortConfiguration removed

--- server/src/platform/mock/MockNetworkUtility.js
+++ server/src/platform/mock/MockNetworkUtility.js
@@ -2,12 +2,6 @@ const INetworkUtility = require('../interfaces/INetworkUtility');
 const logger = require('../../util/Logger');
 
 class MockNetworkUtility extends INetworkUtility {
-  applyPortConfiguration() {
-    return new Promise((resolve) => {
-      resolve();
-    });
-  }
-
   async applyNetworkConfiguration(/* config */) {
     logger.info('mock applyNetworkConfiguration');
   }
--- server/test/unit/platform/interfaces/INetworkUtility.test.js
+++ server/test/unit/platform/interfaces/INetworkUtility.test.js
@@ -2,12 +2,9 @@ const INetworkUtility = require('../../../../src/platform/interfaces/INetworkUti
 
 describe('INetworkUtility interface test', () => {
   it('should throw error when calling methods directly', async () => {
-    expect.assertions(2);
+    expect.assertions(1);
 
     const iNetworkUtility = new INetworkUtility();
-    expect(() => {
-      iNetworkUtility.applyPortConfiguration();
-    }).toThrow(Error);
 
     await iNetworkUtility.applyNetworkConfiguration().catch((e) => expect(e.toString()).toMatch(/Error/));
   });
