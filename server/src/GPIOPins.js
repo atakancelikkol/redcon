e5ab15bdfe2fb642f85ede723091d1fa145d77d0
Deniz Kinay <deniz.kinay@redpine.com.tr>
2020-07-08 16:40:46 +0300

    GPIO pin configurations should be defined platform-specific task
    # a mock pin definition has been created, can be deleted later

--- server/src/GPIOPins.js
+++ server/src/GPIOPins.js
@@ -1,4 +1,6 @@
-const pinDefinitions = {
+const ServerConfig = require('./ServerConfig')
+
+const raspberryPinDef = {
   // definitions for relays
   RELAY_POWER_PIN: 3,
   RELAY_CONTACT_PIN: 5,
@@ -9,4 +11,17 @@ const pinDefinitions = {
   KVM_TOGGLE_PIN: 33,
 };
 
-module.exports = pinDefinitions;
+const otherPinDef = {
+  // definitions for relays
+  RELAY_POWER_PIN: 0,
+  RELAY_CONTACT_PIN: 0,
+
+  // definitions for kvm switch
+  KVM_LED_RPI: 0,
+  KVM_LED_ECU: 0,
+  KVM_TOGGLE_PIN: 0,
+};
+
+const GPIOPins = { raspberry: raspberryPinDef , other: otherPinDef };
+
+module.exports = GPIOPins[ServerConfig.GPIOPinConfigName];
