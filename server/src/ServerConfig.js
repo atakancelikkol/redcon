d1432e3e045ce3b2b4c8ec3f592f420bdeb4c5cb
Atakan Celikkol <atakan.celikkol@redpine.local>
2020-07-10 09:45:19 +0300

    linter error in ServerConfig fixed

--- server/src/ServerConfig.js
+++ server/src/ServerConfig.js
@@ -1,7 +1,7 @@
 const ServerConfig = {
   // Authenticator
   AuthenticatorTokenSecret: '3970B5079E5B3CB468355D16E4E8648EA1BEA982109B813F7B183F42E416E6C5',
-  AuthenticatorTimeoutDuration: 10*60, // units of seconds for now
+  AuthenticatorTimeoutDuration: 10 * 60, // units of seconds for now
 
   // Logger
   LoggerLevel: 'debug', // importance: error > warning > info > verbose > debug > silly
