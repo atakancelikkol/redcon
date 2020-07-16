5df1be4871eeff9f6cdb704a65bb0ddf1ae6cb7e
Orçun Yeniay <orcun.yeniay@redpine.local>
2020-07-16 10:27:12 +0300

    typo fix

--- server/test/unit/platform/mock/MockNetworkUtility.test.js
+++ server/test/unit/platform/mock/MockNetworkUtility.test.js
@@ -1,12 +1,12 @@
 const MockNetworkUtility = require('../../../../src/platform/mock/MockNetworkUtility');
 
 describe('MockNetworkUtility test', () => {
-  it('applyNetworkConfiguration should resolve withtout error', () => {
+  it('applyNetworkConfiguration should resolve without error', () => {
     const mockNetworkUtility = new MockNetworkUtility();
     return expect(mockNetworkUtility.applyNetworkConfiguration()).resolves.toBe();
   });
 
-  it('removeNetworkConfiguration should resolve withtout error', () => {
+  it('removeNetworkConfiguration should resolve without error', () => {
     const mockNetworkUtility = new MockNetworkUtility();
     return expect(mockNetworkUtility.removeNetworkConfiguration()).resolves.toBe();
   });
