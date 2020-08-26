789e620612f7776f82bf90d390ea4dd9aadd220b
Bünyamin Dökmetaş <bunyamin.dokmetas@gmail.com>
2020-08-26 13:49:08 +0300

    added another scenario to test

--- server/test/unit/ControllerPlugins.test.js
+++ server/test/unit/ControllerPlugins.test.js
@@ -3,17 +3,26 @@ const ControllerPlugins = require('../../src/ControllerPlugins');
 const controllerPlugins = new ControllerPlugins();
 
 describe('ControllerBase', () => {
+  it('init', () => {
+    const tempcreateControllerInstances = controllerPlugins.createControllerInstances;
+    controllerPlugins.createControllerInstances = jest.fn();
+    controllerPlugins.init();
+    expect(controllerPlugins.createControllerInstances).toHaveBeenCalledTimes(1);
+    controllerPlugins.createControllerInstances = tempcreateControllerInstances;
+  });
+
   it('should return the name of js files under given folder', () => {
     expect(controllerPlugins.findControllers(__dirname)).toContain(__filename);
     expect(controllerPlugins.findControllers()).not.toContain(['./TestPackageObject.js']);
   });
 
   it('loadModules', () => {
-    expect(controllerPlugins.loadModules(['./ControllerPlugins.js'])).toContain(require('../../src/ControllerPlugins.js'));
+    expect(controllerPlugins.loadModules(['./ControllerPlugins.js'])).toContain(require('../../src/ControllerPlugins.js')); // eslint-disable-line
     expect(controllerPlugins.loadModules()).toContainEqual(expect.anything());
   });
 
   it('createControllerInstances', () => {
     expect(controllerPlugins.createControllerInstances()[0]).toHaveProperty('dataStorage');
+    expect(controllerPlugins.createControllerInstances(controllerPlugins.loadModules())[0]).toHaveProperty('httpServer');
   });
 });
