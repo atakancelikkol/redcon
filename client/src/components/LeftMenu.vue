67efdc1829e01e0a8f8be428aade0b14efe905f0
Bünyamin Dökmetaş <bunyamin.dokmetas@gmail.com>
2020-08-27 13:38:49 +0300

    cleared the code

--- client/src/components/LeftMenu.vue
+++ client/src/components/LeftMenu.vue
@@ -2,38 +2,6 @@
   <div class="left-menu-container">
     <b-list-group>
       <b-list-group-item>REDCON</b-list-group-item>
-      <!---
-      <b-list-group-item>
-        <router-link to="/">
-          Home
-        </router-link>
-      </b-list-group-item>
-      <b-list-group-item>
-        <router-link to="/board-control">
-          Board Control
-        </router-link>
-      </b-list-group-item>
-      <b-list-group-item>
-        <router-link to="/network-config">
-          Network Config
-        </router-link>
-      </b-list-group-item>
-      <b-list-group-item>
-        <router-link to="/serial-console">
-          Serial Console
-        </router-link>
-      </b-list-group-item>
-      <b-list-group-item>
-        <router-link to="/usb-storage">
-          USB Storage
-        </router-link>
-      </b-list-group-item>
-      <b-list-group-item>
-        <router-link to="/utility">
-          Utility
-        </router-link>
-      </b-list-group-item>
-      -->
       <div
         v-for="post in posts"
         :key="post.id"
